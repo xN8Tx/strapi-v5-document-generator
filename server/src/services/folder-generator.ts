cd475d35294d187422ef4aa775d4c5e1d13415ad
xN8Tx <theeldern8t@ya.ru>
2025-01-08 16:19:30 +0700

    feat: add plugin upload folder

--- server/src/services/folder-generator.ts
+++ server/src/services/folder-generator.ts
@@ -1,9 +1,11 @@
-import { PLUGIN_ID } from '../constants';
+import type { Core } from '@strapi/strapi';
 
 import fs from 'fs/promises';
 import path from 'path';
 
-export default () => ({
+import { PLUGIN_ID } from '../constants';
+
+export default ({ strapi }: { strapi: Core.Strapi }) => ({
   async generatePluginFolder() {
     const folderPath = path.join(strapi.dirs.app.extensions, PLUGIN_ID);
     try {
@@ -19,5 +21,27 @@ export default () => ({
     } catch {
       await fs.mkdir(folderPath);
     }
+  },
+  async generateUploadPluginFolder() {
+    const currentFolders = await strapi.documents('plugin::upload.folder').findMany({
+      sort: { pathId: 'asc' },
+    });
+
+    if (currentFolders.find((f) => f.name === PLUGIN_ID)) {
+      return;
+    }
+
+    let pathId = 1;
+    if (currentFolders.length > 0) {
+      pathId = currentFolders[currentFolders.length - 1].pathId + 1;
+    }
+
+    await strapi.documents('plugin::upload.folder').create({
+      data: {
+        name: PLUGIN_ID,
+        path: '/' + PLUGIN_ID,
+        pathId,
+      },
+    });
   },
 });
