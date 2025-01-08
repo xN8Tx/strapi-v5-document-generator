ea99a3f98991b579ae7d39516c10b262c108d87f
xN8Tx <theeldern8t@ya.ru>
2025-01-08 14:21:00 +0700

    fix: add closures to _scenarios in scenario service

--- server/src/services/scenario.ts
+++ server/src/services/scenario.ts
@@ -1,31 +1,33 @@
-import type { Core } from '@strapi/strapi';
+// import type { Core } from '@strapi/strapi';
 import { ScenarioType } from '../types';
 
-export default ({ strapi }: { strapi: Core.Strapi }) => ({
-  _scenarios: [],
-  push(scenarioOrArray: ScenarioType | ScenarioType[]) {
-    const scenarios = Array.isArray(scenarioOrArray) ? scenarioOrArray : [scenarioOrArray];
+export default (/*{ strapi }: { strapi: Core.Strapi }*/) => {
+  const _scenarios: ScenarioType[] = [];
+  return {
+    push(scenarioOrArray: ScenarioType | ScenarioType[]) {
+      const scenarios = Array.isArray(scenarioOrArray) ? scenarioOrArray : [scenarioOrArray];
 
-    scenarios.forEach((scenario) => {
-      if (
-        !scenario.id ||
-        !scenario.title ||
-        !scenario.templateSlug ||
-        typeof scenario.getContent !== 'function'
-      ) {
-        throw new Error(
-          'Invalid scenario: "id", "title", "templateSlug", and "getContent" are required.'
-        );
-      }
+      scenarios.forEach((scenario) => {
+        if (
+          !scenario.id ||
+          !scenario.title ||
+          !scenario.templateSlug ||
+          typeof scenario.getContent !== 'function'
+        ) {
+          throw new Error(
+            'Invalid scenario: "id", "title", "templateSlug", and "getContent" are required.'
+          );
+        }
 
-      if (this._scenarios.find((s) => s.id === scenario.id)) {
-        throw new Error(`Scenario with id "${scenario.id}" already exists.`);
-      }
+        if (_scenarios.find((s) => s.id === scenario.id)) {
+          throw new Error(`Scenario with id "${scenario.id}" already exists.`);
+        }
 
-      this._scenarios.push(scenario);
-    });
-  },
-  get() {
-    return this._scenarios;
-  },
-});
+        _scenarios.push(scenario);
+      });
+    },
+    get() {
+      return _scenarios;
+    },
+  };
+};
